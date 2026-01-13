03ce8129b7633186226dacb14d15e738a65f1ed5
AbhayR-Hestabit <abhay.rajput@hestabit.in>
2026-01-13 13:45:21 +0530

    changed crontab

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-var x=
+function getTimestamp() {
+  return new Date().toISOString();
+}
+
+function logStartup() {
+  const message = `Application started at ${getTimestamp()}`;
+  console.log(message);
+  return message;
+}
+
+logStartup();
